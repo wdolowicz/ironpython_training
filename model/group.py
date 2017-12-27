855549d0cb15a453fda81d06984b624be9b6c314
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2017-12-27 15:39:50 +0100

    Created model group

---
+++ model/group.py
@@ -0,0 +1,15 @@
+__author__ = 'wdolowicz'
+
+
+class Group:
+    def __init__(self, name=None):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def key(self):
+        return self.name
