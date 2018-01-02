d3b0b0be8864391158d1d284856dd32cd517bf74
wdolowicz <33247201+wdolowicz@users.noreply.github.com>
2018-01-02 13:47:01 +0100

    Parametrized test to use excel data

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,9 @@
 __author__ = 'wdolowicz'
 
-from model.group import Group
 
-
-def test_add_group(app):
+def test_add_group(app, xlsx_groups):
+    group = xlsx_groups
     old_list = app.group.get_group_list(app.main_window)
-    group = Group(name='test')
     app.group.add_new_group(app.main_window, group)
     new_list = app.group.get_group_list(app.main_window)
     old_list.append(group.name)
